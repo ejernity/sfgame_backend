ad04a31a48b68094c2208b8d0db542cd393ced60
ejernity <nikoslamprinos13@gmail.com>
2023-05-09 13:03:08 +0300

    Initial commit

---
+++ src/main/java/gr/nlamp/sfgame_backend/SfgameBackendApplication.java
@@ -0,0 +1,13 @@
+package gr.nlamp.sfgame_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgameBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgameBackendApplication.class, args);
+    }
+
+}
